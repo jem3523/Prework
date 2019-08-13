726f17585ae626277558fdd9399a3c9e79b3a75d
Jason Miller <50375852+jem3523@users.noreply.github.com>
2019-08-13 10:24:19 -0500

    Add files via upload

---
+++ Prework/module_06/javascript.js
@@ -0,0 +1,26 @@
+document.getElementById("GrowButton").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+    
+    });
+    
+document.getElementById("BlueButton").addEventListener("click", function(){
+    
+    document.getElementById("box").style.backgroundColor = "blue";
+    
+    });
+    
+document.getElementById("FadeButton").addEventListener("click", function(){
+    
+    document.getElementById("box").style.backgroundColor = "rgb(50, 115, 220)";
+    
+    });
+    
+    
+document.getElementById("ResetButton").addEventListener("click", function(){
+    
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    
+    });
+    
